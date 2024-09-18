5465cba888347071d2bb14061fe7148f6875a702
Sophia Marques Contesini <sophiamarques214@gmail.com>
2024-09-18 10:58:57 -0300

    feat: implementa front-end web

---
+++ web/src/main.tsx
@@ -0,0 +1,13 @@
+import { createRoot } from 'react-dom/client'
+import { App } from './App'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+import './index.css'
+
+const queryClient = new QueryClient()
+
+createRoot(document.getElementById('root')!).render(
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>
+)
